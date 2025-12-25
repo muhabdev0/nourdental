75757e12e861bb6ba1d6491fba70a792817e2c8c
Muhab <muhabdev0@gmail.com>
2025-12-25 16:41:54 +0000

    use cairo font fot the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"Playfair Display"', 'serif'],
+        body: ['"Cairo"', 'sans-serif'],
+        headline: ['"Cairo"', 'serif'],
         code: ['monospace'],
       },
       colors: {
