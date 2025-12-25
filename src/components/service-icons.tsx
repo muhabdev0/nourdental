13082766019eca0be10bc6733903f02c74fcffe3
Muhab <muhabdev0@gmail.com>
2025-12-25 17:37:09 +0000

    use proper icons from internet on خدماتنا

--- src/components/service-icons.tsx
+++ src/components/service-icons.tsx
@@ -2,110 +2,84 @@ import type { SVGProps } from "react";
 
 export const GeneralDentistry = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <path d="M9.06 2.24A6.94 6.94 0 0 1 12 1a6.94 6.94 0 0 1 2.94 1.24" />
-    <path d="M12 15.36V23" />
-    <path d="M16 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M8 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M7 15.36c-3.14-.59-5-2.92-5-5.99 0-3.53 2.84-6.37 6.33-6.37 1.48 0 2.85.5 3.94 1.35" />
-    <path d="M17 15.36c3.14-.59 5-2.92 5-5.99 0-3.53-2.84-6.37-6.33-6.37-1.48 0-2.85.5-3.94 1.35" />
+    <path d="M12 2a5.5 5.5 0 0 0-5.5 5.5c0 2.25 1.25 4.15 3 5.08V18h5v-5.42c1.75-.93 3-2.83 3-5.08A5.5 5.5 0 0 0 12 2Z" />
+    <path d="M12 18v4" />
+    <path d="M9 22h6" />
   </svg>
 );
 
 export const TeethCleaning = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <path d="M9.06 2.24A6.94 6.94 0 0 1 12 1a6.94 6.94 0 0 1 2.94 1.24" />
-    <path d="M12 15.36V23" />
-    <path d="M16 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M8 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M7 15.36c-3.14-.59-5-2.92-5-5.99 0-3.53 2.84-6.37 6.33-6.37 1.48 0 2.85.5 3.94 1.35" />
-    <path d="M17 15.36c3.14-.59 5-2.92 5-5.99 0-3.53-2.84-6.37-6.33-6.37-1.48 0-2.85.5-3.94 1.35" />
-    <path d="m2 12 2 2 2-2" />
-    <path d="m18 12 2 2 2-2" />
+    <path d="M12 2a5.5 5.5 0 0 0-5.5 5.5c0 2.25 1.25 4.15 3 5.08V18h5v-5.42c1.75-.93 3-2.83 3-5.08A5.5 5.5 0 0 0 12 2Z" />
+    <path d="M12 18v4" />
+    <path d="M9 22h6" />
+    <path d="M2 12s2-2 4-2 4 2 4 2" />
+    <path d="M14 12s2-2 4-2 4 2 4 2" />
+    <path d="M10 12s2 2 4 2" />
   </svg>
 );
 
 export const TeethWhitening = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <path d="M9.06 2.24A6.94 6.94 0 0 1 12 1a6.94 6.94 0 0 1 2.94 1.24" />
-    <path d="M12 15.36V23" />
-    <path d="M16 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M8 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M7 15.36c-3.14-.59-5-2.92-5-5.99 0-3.53 2.84-6.37 6.33-6.37 1.48 0 2.85.5 3.94 1.35" />
-    <path d="M17 15.36c3.14-.59 5-2.92 5-5.99 0-3.53-2.84-6.37-6.33-6.37-1.48 0-2.85.5-3.94 1.35" />
-    <path d="M12 5.5v-3" />
-    <path d="m5.5 7-2-2" />
-    <path d="m18.5 7 2-2" />
+    <path d="M12 2a5.5 5.5 0 0 0-5.5 5.5c0 2.25 1.25 4.15 3 5.08V18h5v-5.42c1.75-.93 3-2.83 3-5.08A5.5 5.50 0 0 0 12 2Z" />
+    <path d="M12 18v4" />
+    <path d="M9 22h6" />
+    <path d="M6 13h.01" />
+    <path d="M18 13h.01" />
+    <path d="M14.5 9h.01" />
+    <path d="M9.5 9h.01" />
+    <path d="M12 5h.01" />
   </svg>
 );
 
 export const RootCanal = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <path d="M9.06 2.24A6.94 6.94 0 0 1 12 1a6.94 6.94 0 0 1 2.94 1.24" />
-    <path d="M12 15.36V23" />
-    <path d="M16 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M8 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M7 15.36c-3.14-.59-5-2.92-5-5.99 0-3.53 2.84-6.37 6.33-6.37 1.48 0 2.85.5 3.94 1.35" />
-    <path d="M17 15.36c3.14-.59 5-2.92 5-5.99 0-3.53-2.84-6.37-6.33-6.37-1.48 0-2.85.5-3.94 1.35" />
-    <path d="M10 18h4" />
-    <path d="m11 23 3-5" />
-    <path d="m13 23-3-5" />
+    <path d="M12 2a5.5 5.5 0 0 0-5.5 5.5c0 2.25 1.25 4.15 3 5.08V18h5v-5.42c1.75-.93 3-2.83 3-5.08A5.5 5.5 0 0 0 12 2Z" />
+    <path d="M12 18v4" />
+    <path d="M9 22h6" />
+    <path d="M12 11.5v-1" />
+    <path d="m14 14-2-3-2 3" />
   </svg>
 );
 
 export const Orthodontics = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <path d="M9.06 2.24A6.94 6.94 0 0 1 12 1a6.94 6.94 0 0 1 2.94 1.24" />
-    <path d="M12 15.36V23" />
-    <path d="M16 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M8 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M7 15.36c-3.14-.59-5-2.92-5-5.99 0-3.53 2.84-6.37 6.33-6.37 1.48 0 2.85.5 3.94 1.35" />
-    <path d="M17 15.36c3.14-.59 5-2.92 5-5.99 0-3.53-2.84-6.37-6.33-6.37-1.48 0-2.85.5-3.94 1.35" />
-    <line x1="5" x2="19" y1="9" y2="9" />
-    <line x1="10.5" x2="10.5" y1="9" y2="12" />
-    <line x1="13.5" x2="13.5" y1="9" y2="12" />
+    <path d="M12 2a5.5 5.5 0 0 0-5.5 5.5c0 2.25 1.25 4.15 3 5.08V18h5v-5.42c1.75-.93 3-2.83 3-5.08A5.5 5.5 0 0 0 12 2Z" />
+    <path d="M12 18v4" />
+    <path d="M9 22h6" />
+    <path d="M6 9h12" />
+    <path d="M9 6v6" />
+    <path d="M15 6v6" />
   </svg>
 );
 
 export const PediatricDentistry = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <path d="M7.33 2.24A5 5 0 0 1 9.5 1a5 5 0 0 1 2.17 1.24" />
-    <path d="M9.5 11.36V16" />
-    <path d="M12 8.36V5.5a1 1 0 0 0-2 0v2.86" />
-    <path d="M7 8.36V5.5a1 1 0 0 0-2 0v2.86" />
-    <path d="M5.5 11.36C3.21 10.94 2 9.15 2 7.01 2 A4.02 4.02 0 0 1 6.02 3c1.1 0 2.09.43 2.81.9" />
-    <path d="M13.5 11.36c2.29-.42 3.5-2.21 3.5-4.35A4.02 4.02 0 0 0 12.98 3c-1.1 0-2.09.43-2.81.9" />
-    <path d="M22 17c-2 2-5 2-7 0" />
-    <path d="M15 17c2 2 5 2 7 0" />
-    <path d="M12 18s-2 2-4 0" />
-    <path d="M4 18s2 2 4 0" />
+    <path d="M12 2a5.5 5.5 0 0 0-5.5 5.5c0 2.25 1.25 4.15 3 5.08V18h5v-5.42c1.75-.93 3-2.83 3-5.08A5.5 5.5 0 0 0 12 2Z" />
+    <path d="M12 18v4" />
+    <path d="M9 22h6" />
+    <path d="M15 5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Z" />
+    <path d="M9 5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Z" />
+    <path d="M15 13a3 3 0 0 0-6 0" />
   </svg>
 );
 
 export const DentalImplants = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <path d="M9.06 2.24A6.94 6.94 0 0 1 12 1a6.94 6.94 0 0 1 2.94 1.24" />
-    <path d="M12 15.36c-2 0-3.8-1-4.94-2.65" />
-    <path d="M16 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M12 15.36c2 0 3.8-1 4.94-2.65" />
-    <path d="M7 15.36c-3.14-.59-5-2.92-5-5.99 0-3.53 2.84-6.37 6.33-6.37 1.48 0 2.85.5 3.94 1.35" />
-    <path d="M17 15.36c3.14-.59 5-2.92 5-5.99 0-3.53-2.84-6.37-6.33-6.37-1.48 0-2.85.5-3.94 1.35" />
-    <path d="M12 15.36v7.14" />
-    <path d="M9.5 18.5h5" />
-    <path d="M8 22.5h8" />
+    <path d="M12 2a5.5 5.5 0 0 0-5.5 5.5c0 2.25 1.25 4.15 3 5.08V18h5v-5.42c1.75-.93 3-2.83 3-5.08A5.5 5.5 0 0 0 12 2Z" />
+    <path d="M12 18v-2" />
+    <path d="M12 22v-2" />
+    <path d="M9 16h6" />
+    <path d="M9 20h6" />
   </svg>
 );
 
 export const Emergency = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <path d="M9.06 2.24A6.94 6.94 0 0 1 12 1a6.94 6.94 0 0 1 2.94 1.24" />
-    <path d="M12 15.36V23" />
-    <path d="M16 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M8 11.36V6.5a1.5 1.5 0 0 0-3 0v4.86" />
-    <path d="M7 15.36c-3.14-.59-5-2.92-5-5.99 0-3.53 2.84-6.37 6.33-6.37 1.48 0 2.85.5 3.94 1.35" />
-    <path d="M17 15.36c3.14-.59 5-2.92 5-5.99 0-3.53-2.84-6.37-6.33-6.37-1.48 0-2.85.5-3.94 1.35" />
-    <path d="M18 6h-2" />
-    <path d="M17 5v2" />
-    <path d="M5 6h2" />
-    <path d="M6 5v2" />
+    <path d="m10.14 6.94.86-2.9 8 8-2.9.86" />
+    <path d="m14 10-8-8 2.86-.86.86 2.9" />
+    <path d="M12 2a5.5 5.5 0 0 0-5.5 5.5c0 2.25 1.25 4.15 3 5.08V18h5v-5.42c1.75-.93 3-2.83 3-5.08A5.5 5.5 0 0 0 12 2Z" />
+    <path d="M12 18v4" />
+    <path d="M9 22h6" />
   </svg>
 );
