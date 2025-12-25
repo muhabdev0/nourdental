ba510244d2f2e4f070db104bd318184d5feaef4a
Muhab <muhabdev0@gmail.com>
2025-12-25 16:37:25 +0000

    add this link for the image : https://fakahanyclinic.com/wp-content/uplo

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'fakahanyclinic.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
